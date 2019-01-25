1e497865935c35cfbf6791ffd204f9aa678ce0d7
reclans <noelrecla@hotmail.com>
2019-01-25 11:01:37 -0500

    testing buddy github settings/protections

--- presqt/models.py
+++ presqt/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# *****may want to move this up one level, but should work here for all Django apps
+# *****@@may want to move this up one level, but should work here for all Django apps
 class User(AbstractUser):
     """extending Django user class"""
 
     title = models.CharField(max_length=30, blank=True)
+    test_field = models.CharField(max_length=3, blank=True)
