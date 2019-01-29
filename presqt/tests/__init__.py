8db315db851d37dbe3d9fe1a256c4359095e68ae
reclans <noelrecla@hotmail.com>
2019-01-29 16:25:16 -0500

    testing buddy github updating branch by updating unit tests so they fail

--- presqt/tests/__init__.py
+++ presqt/tests/__init__.py
@@ -5,6 +5,6 @@ class BuddyGithubTestCase(TestCase):
 
     def test_fail_fail_dont_work(self):
         x = 0
-        self.assertEqual(x, 0)
+        self.assertEqual(x, 1)
         y = 3
         self.assertEqual(y, 3)
