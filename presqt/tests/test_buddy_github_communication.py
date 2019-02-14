1c837265f39c095266256ab5de2d2280a14830f0
reclans <10Warhammer10>
2019-02-14 13:32:15 -0500

    Setting up Git so master branch cannot be directly pushed or merged to ideally without a review.  Making sure a pull request can be merged in to master branch.

--- presqt/tests/test_buddy_github_communication.py
+++ presqt/tests/test_buddy_github_communication.py
@@ -4,5 +4,7 @@ from django.test import TestCase
 class BuddyGithubTestCase(TestCase):
 
     def test_fail_fail_dont_work(self):
+        y = 4
+        self.assertEqual(y, 4)
         y = 3
         self.assertEqual(y, 3)
