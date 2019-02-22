ba391be94f241bee850d90dc64751eca310e96d9
bfox6 <bfox6@nd.edu>
2019-02-22 09:35:27 -0500

    Removed stale commands

--- presqt/osf/management/commands/osf.py
+++ presqt/osf/management/commands/osf.py
@@ -8,7 +8,7 @@ from presqt.osf.functions.functions import fetch_resources, fetch_resource
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # fetch_resources(OSF_TOKEN)
+        fetch_resources(OSF_TOKEN)
         print(fetch_resource(OSF_TOKEN, 'https://api.osf.io/v2/nodes/gq92a/', 'project'))
         print(fetch_resource(OSF_TOKEN, 'https://api.osf.io/v2/files/5c5dfe2d22b04e001c2c289b/', 'file'))
         print(fetch_resource(OSF_TOKEN, 'https://api.osf.io/v2/files/5c61a50ce16f5500188f088a/', 'folder'))
