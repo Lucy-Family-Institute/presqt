9df493e1e44f49323b7a3e1d1ba0439622783ef0
Justin Branco <jbranco@nd.edu>
2019-06-11 12:45:58 +0100

    Very basic delete_mediafiles command

---
+++ presqt/management/commands/delete_mediafiles.py
@@ -0,0 +1,30 @@
+
+from dateutil.parser import parse
+from datetime import datetime
+import glob
+import json
+import shutil
+
+from django.core.management import BaseCommand
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        """
+        Delete all mediafiles that have run past their
+        expiration date.
+        """
+        for directory in glob.glob('mediafiles/downloads/*/'):
+            for metadata in glob.glob(directory + 'server_metadata.json'):
+                with open(metadata) as server_metadata:
+                    data = json.load(server_metadata)
+                    # Convert string datetime into a datetime object
+                    expiration = parse(data['expiration'])
+
+                    if expiration <= datetime.now():
+                        shutil.rmtree(directory)
+                        print(directory + ' has been deleted.')
+
+                    else:
+                        print(directory + ' has been retained.')
