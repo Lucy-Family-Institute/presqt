f3a0ee18f6d181a543f216d41b08974575f67cd7
Justin Branco <jbranco@nd.edu>
2019-06-12 10:37:06 +0100

    Add DownloadResource to __init__

--- presqt/api_v1/views/resource/__init__.py
+++ presqt/api_v1/views/resource/__init__.py
@@ -1,9 +1,11 @@
 from presqt.api_v1.views.resource.resource import Resource
 from presqt.api_v1.views.resource.resource_collection import ResourceCollection
-from presqt.api_v1.views.resource.resource_download import PrepareDownload
+from presqt.api_v1.views.resource.resource_download import (
+    PrepareDownload, DownloadResource)
 
 __all__ = [
+    DownloadResource,
+    PrepareDownload,
     Resource,
-    ResourceCollection,
-    PrepareDownload
+    ResourceCollection
 ]
