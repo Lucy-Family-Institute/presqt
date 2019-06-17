4ff8035e0eaa47918bff458c1231e1a37e29597c
bfox6 <bfox6@nd.edu>
2019-06-17 15:55:04 -0400

    Removed file. Added command ot docker compose

--- docker/django_entrypoint.sh
+++ docker/django_entrypoint.sh
@@ -13,6 +13,7 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 
 python manage.py validate_target_json
+python manage.py delete_mediafiles
 
 if [ "$ENVIRONMENT" = "development" ]
 then
