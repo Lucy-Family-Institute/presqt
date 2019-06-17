8bb8aa706a69b3e1ae940865f4f14ebddc4043af
Justin Branco <jbranco@nd.edu>
2019-06-17 16:54:47 +0100

    Minor formatting tweaks

--- presqt/management/tests/commands/test_delete_mediafiles.py
+++ presqt/management/tests/commands/test_delete_mediafiles.py
@@ -5,10 +5,12 @@ import os
 from dateutil.relativedelta import relativedelta
 
 from django.core.management import call_command
+from django.conf import settings
 from django.test.testcases import TestCase
 from django.utils import timezone
 
 from presqt.api_v1.views.resource.resource_download import download_resource
+from presqt.api_v1.utilities.io.read_file import read_file
 
 
 class TestDeleteMediaFiles(TestCase):
@@ -16,18 +18,18 @@ class TestDeleteMediaFiles(TestCase):
         self.directory = 'mediafiles/downloads/test_command/'
         if not os.path.exists(self.directory):
             os.makedirs(self.directory)
-        self.data = ({"presqt-source-token": "blahblah", "status": "finished",
+        self.data = ({"presqt-source-token": "blahblah", "status": "in_progress",
                       "expiration": str(timezone.now()+relativedelta(days=5)),
                       "kill_time": "2019-06-13 15:38:18.229257+00:00",
-                      "message": "Download successful", "status_code": "200", "zip_name": "test.zip"})
+                      "message": "Download successful", "status_code": "200",
+                      "zip_name": "test.zip"})
         with open('{}process_info.json'.format(self.directory), 'w+') as file:
             json.dump(self.data, file)
 
         # Download file into test directory
-        download_resource('osf', 'resource_download',
-                          '0UAX3rbdd59OUXkGIu19gY0BMQODAbtGimcLNAfAie6dUQGGPig93mpFOpJQ8ceynlGScp',
-                          '5cd98978f244ec001ee86609', self.directory,
-                          '{}process_info.json'.format(self.directory))
+        download_resource('osf', 'resource_download', settings.PRIVATE_USER_TOKEN,
+                          '5cd98978f244ec001ee86609', self.directory, '{}process_info.json'.format(
+                              self.directory))
 
     def test_files_to_be_retained(self):
         """
