c3ce39501bc5835790c7c0a498f2528c5ba23bb2
Justin Branco <jbranco@nd.edu>
2019-08-27 13:21:55 -0400

    Test tweak

--- presqt/targets/osf/utilities/utils/delete_users_projects.py
+++ presqt/targets/osf/utilities/utils/delete_users_projects.py
@@ -16,7 +16,7 @@ def delete_users_projects(auth_token):
     for node in response['data']:
         requests.delete('http://api.osf.io/v2/nodes/{}'.format(node['id']), headers=headers)
     next_url = response['links']['next']
-    while next_url is not None:
+    while next_url is not None:  # pragma: no cover
         response_json = requests.get('http://api.osf.io/v2/users/me/nodes', headers=headers).json()
         for node in response_json['data']:
             requests.delete('http://api.osf.io/v2/nodes/{}'.format(node['id']), headers=headers)
