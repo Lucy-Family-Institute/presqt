c9dc6f5049bfc19997b8627660e7de9b87df1e4e
Justin Branco <jbranco@nd.edu>
2019-09-18 15:27:05 -0400

    Remove header

--- presqt/targets/github/utilities/helpers/get_page_total.py
+++ presqt/targets/github/utilities/helpers/get_page_total.py
@@ -16,7 +16,7 @@ def get_page_total(token):
     The number of pages of repos the user has.
     """
     user_url = 'https://api.github.com/user?access_token={}'.format(token)
-    user_data = requests.get(user_url, headers=header).json()
+    user_data = requests.get(user_url).json()
     public_repos = user_data['public_repos']
     private_repos = user_data['total_private_repos']
     total_repos = public_repos + private_repos
