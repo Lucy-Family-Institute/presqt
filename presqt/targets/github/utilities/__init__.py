1252087cfee5b8ed79b2ee0f0619dbfc1d752032
Justin Branco <jbranco@nd.edu>
2019-09-23 14:53:49 -0400

    Delete function for tests

--- presqt/targets/github/utilities/__init__.py
+++ presqt/targets/github/utilities/__init__.py
@@ -3,9 +3,11 @@ from presqt.targets.github.utilities.helpers.download_content import download_co
 from presqt.targets.github.utilities.helpers.get_page_total import get_page_total
 from presqt.targets.github.utilities.helpers.github_paginated_data import github_paginated_data
 from presqt.targets.github.utilities.helpers.validation_check import validation_check
+from presqt.targets.github.utilities.utils.delete_github_repo import delete_github_repo
 
 all = [
     download_content,
+    delete_github_repo,
     get_page_total,
     github_paginated_data,
     validation_check,
