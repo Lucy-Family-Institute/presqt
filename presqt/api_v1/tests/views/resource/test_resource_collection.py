f92cbd36eee6d37a8a3ee70fbd78e521d0e9dc90
Justin Branco <jbranco@nd.edu>
2020-03-14 13:23:25 -0400

    Update tests

--- presqt/api_v1/tests/views/resource/test_resource_collection.py
+++ presqt/api_v1/tests/views/resource/test_resource_collection.py
@@ -65,7 +65,7 @@ class TestResourceCollection(SimpleTestCase):
         If get_action_message is called and fixity and metadata has failed, we need to make the user
         aware.
         """
-        error_message = get_action_message('Download', False, False)
+        error_message = get_action_message('Download', False, False, {})
 
         self.assertEqual(error_message, 'Download successful but with fixity and metadata errors.')
 
