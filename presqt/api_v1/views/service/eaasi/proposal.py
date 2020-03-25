464d4c8c4534ef5d9d03a5fee1e32fed24ad76a0
bfox6 <bfox6@nd.edu>
2020-03-25 12:49:35 -0400

    Added some eaasi things

--- presqt/api_v1/views/service/eaasi/proposal.py
+++ presqt/api_v1/views/service/eaasi/proposal.py
@@ -1,4 +1,6 @@
 import json
+from uuid import uuid4
+
 import requests
 
 from rest_framework.views import APIView
@@ -6,31 +8,39 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import status
 
+from presqt.api_v1.utilities import get_process_info_data
+from presqt.utilities import write_file
+
 
 class Proposal(APIView):
     """
     Handles shared POST requests to EaaSI api.
     """
 
-    def post(self, request, ticket_number=None):
+    def post(self, request):
         """
         Upload resources to an EaaSI node.
 
         Parameters
         ----------
         request : HTTP Request Object
-        ticket_number : str
-            The ticket number of the zip to pass along.
         """
-        data = {
-            "data_url": "https://presqt.readthedocs.io/en/latest/_downloads/cca126b15be422e27bfe28e0b7f994db/NewProjectWithFolderBagIt.zip",
-            "data_type": "bagit+zip"}
+        ticket_number = request.data['ticket_number']
+        eaasi_token = str(uuid4())
+        data = get_process_info_data('downloads', ticket_number)
+        data['eaasi_token'] = eaasi_token
+        write_file('mediafiles/downloads/{}/process_info.json'.format(ticket_number), data, True)
 
-        response = requests.post(
-            'https://eaasi-portal.emulation.cloud/environment-proposer/api/v1/proposals',
-            data=json.dumps(data),
-            headers={"Content-Type": "application/json"})
-        
-        return Response(
-                data=response.json(),
-                status=status.HTTP_202_ACCEPTED)
+        download_job_url = reverse('download_job', kwargs={"ticket_number": ticket_number})
+        final_download_job_url = '{}?eaasi_token={}'.format(download_job_url, eaasi_token)
+
+        # data = {
+        #     "data_url": "https://presqt.readthedocs.io/en/latest/_downloads/cca126b15be422e27bfe28e0b7f994db/NewProjectWithFolderBagIt.zip",
+        #     "data_type": "bagit+zip"}
+
+        # response = requests.post(
+        #     'https://eaasi-portal.emulation.cloud/environment-proposer/api/v1/proposals',
+        #     data=json.dumps(data),
+        #     headers={"Content-Type": "application/json"})
+
+        return Response(data={}, status=status.HTTP_202_ACCEPTED)
