26a14bd3d3f6088c5cde5bf125a51dff6730a34b
Justin Branco <jbranco@nd.edu>
2020-03-31 12:16:19 -0400

    Search validator fixed

--- presqt/api_v1/utilities/validation/search_validator.py
+++ presqt/api_v1/utilities/validation/search_validator.py
@@ -19,19 +19,17 @@ def search_validator(search_parameter):
         raise PresQTResponseException('PresQT Error: The search query is not formatted correctly.',
                                       status.HTTP_400_BAD_REQUEST)
 
-    # Check that the query parameter has `title`
-    try:
-        search_parameter['title']
-    except KeyError:
-        try:
-            search_parameter['id']
-        except KeyError:
-            raise PresQTResponseException('PresQT Error: The search query is not formatted correctly.',
-                                          status.HTTP_400_BAD_REQUEST)
+    list_of_search_params = ['id', 'title']
+    # Check that the query parameter is in list of accepted searches
+    if list(search_parameter.keys())[0] in list_of_search_params:
+        pass
+    else:
+        raise PresQTResponseException('PresQT Error: The search query is not formatted correctly.',
+                                      status.HTTP_400_BAD_REQUEST)
 
     # Ensure that there are no special characters in the search.
     regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
-    
+
     if 'title' in search_parameter:
         if (regex.search(search_parameter['title']) is not None):
             raise PresQTResponseException('PresQT Error: The search query is not formatted correctly.',
