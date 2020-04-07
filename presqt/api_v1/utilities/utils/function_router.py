b6c28914c25c6c0cb20354843bc7a9dd90fa50b1
bfox6 <bfox6@nd.edu>
2020-04-07 15:28:25 -0400

    Added Gitlab collection and detail

--- presqt/api_v1/utilities/utils/function_router.py
+++ presqt/api_v1/utilities/utils/function_router.py
@@ -6,6 +6,7 @@ from presqt.targets.github.functions.upload_metadata import github_upload_metada
 from presqt.targets.curate_nd.functions.fetch import (
     curate_nd_fetch_resources, curate_nd_fetch_resource)
 from presqt.targets.curate_nd.functions.download import curate_nd_download_resource
+from presqt.targets.gitlab.functions.fetch import gitlab_fetch_resources, gitlab_fetch_resource
 
 from presqt.targets.osf.functions.fetch import osf_fetch_resources, osf_fetch_resource
 from presqt.targets.osf.functions.download import osf_download_resource
@@ -70,3 +71,7 @@ class FunctionRouter(object):
     zenodo_resource_download = zenodo_download_resource
     zenodo_resource_upload = zenodo_upload_resource
     zenodo_metadata_upload = zenodo_upload_metadata
+
+    gitlab_resource_collection = gitlab_fetch_resources
+    gitlab_resource_detail = gitlab_fetch_resource
+
