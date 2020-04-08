f1836b384def60e20d46c0c2f05e061c34cbc190
Justin Branco <jbranco@nd.edu>
2020-04-08 10:51:04 -0400

    Download helpers

--- presqt/targets/gitlab/utilities/gitlab_paginated_data.py
+++ presqt/targets/gitlab/utilities/gitlab_paginated_data.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def gitlab_paginated_data(headers, user_id):
+def gitlab_paginated_data(headers, user_id, url=None):
     """
     For the given user, find and return all project data.
 
@@ -11,20 +11,26 @@ def gitlab_paginated_data(headers, user_id):
         Headers for requests.
     user_id: str
         ID of the GitLab user.
+    url: str
+        URL to call
 
     Returns
     -------
     List of all paginated data.
     """
-    project_url = "https://gitlab.com/api/v4/users/{}/projects".format(user_id)
+    if not url:
+        url = "https://gitlab.com/api/v4/users/{}/projects".format(user_id)
 
     data = []
-    response = requests.get(project_url, headers=headers)
+    response = requests.get(url, headers=headers)
     next_page_number = response.headers['X-Next-Page']
     data.extend(response.json())
 
     while next_page_number:
-        next_url = "{}?page={}".format(project_url, next_page_number)
+        if 'users' in url:
+            next_url = "{}?page={}".format(url, next_page_number)
+        else:
+            next_url = "{}&page={}".format(url, next_page_number)
         next_response = requests.get(next_url, headers=headers)
         data.extend(next_response.json())
         next_page_number = next_response.headers['X-Next-Page']
