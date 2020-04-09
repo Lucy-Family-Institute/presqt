ec15fdbe980f4229e42297c69cfd18b9ff2b4bf0
Justin Branco <jbranco@nd.edu>
2020-04-09 15:09:57 -0400

    Fixed targets tests

--- presqt/api_v1/tests/views/target/test_target.py
+++ presqt/api_v1/tests/views/target/test_target.py
@@ -21,7 +21,7 @@ class TestTargetCollection(SimpleTestCase):
 
         # Verify that the first dictionary in the payload's array has the correct keys
         expected_keys = ['name', 'readable_name', 'supported_actions', 'supported_transfer_partners',
-                         'supported_hash_algorithms', 'infinite_depth', 'links']
+                         'supported_hash_algorithms', 'infinite_depth', 'search_parameters', 'links']
 
         expected_supported_keys = ['resource_collection', 'resource_detail', 'resource_download',
                                    'resource_upload', 'resource_transfer_in', 'resource_transfer_out']
