9039fb4257e9a3266cd2307cb8b24003193da4d8
bfox6 <bfox6@nd.edu>
2020-05-26 10:04:40 -0400

    Added PresQT to details

--- presqt/api_v1/views/resource/base_resource.py
+++ presqt/api_v1/views/resource/base_resource.py
@@ -336,10 +336,10 @@ class BaseResource(APIView):
         if self.action == 'resource_transfer_in':
             source_target_data = get_target_data(self.source_target_name)
             destination_target_data = get_target_data(self.destination_target_name)
-            self.details = "Transfer from {} to {}".format(source_target_data['readable_name'], destination_target_data['readable_name'])
+            self.details = "PresQT transfer from {} to {}".format(source_target_data['readable_name'], destination_target_data['readable_name'])
         else:
             source_target_data = get_target_data(self.source_target_name)
-            self.details = "Download from {}".format(source_target_data['readable_name'])
+            self.details = "PresQT download from {}".format(source_target_data['readable_name'])
 
         self.action_metadata = {
             'id': str(uuid4()),
