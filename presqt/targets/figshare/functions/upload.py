4ec65d06f6bb27cf688dc952aa696d8f89cb8257
Justin Branco <jbranco@nd.edu>
2020-06-29 13:41:41 -0400

    Upload in progress

---
+++ presqt/targets/figshare/functions/upload.py
@@ -0,0 +1,67 @@
+import base64
+import json
+import os
+import requests
+
+from rest_framework import status
+
+from presqt.targets.figshare.utilities.validation_check import validation_check
+from presqt.utilities import PresQTResponseException
+
+
+def figshare_upload_resource(token, resource_id, resource_main_dir, hash_algorithm, file_duplicate_action):
+    """
+    Upload the files found in the resource_main_dir to the target.
+
+    Parameters
+    ----------
+    token : str
+        User's token.
+    resource_id : str
+        ID of the resource requested.
+    resource_main_dir : str
+        Path to the main directory for the resources to be uploaded.
+    hash_algorithm : str
+        Hash algorithm we are using to check for fixity.
+    file_duplicate_action : str
+        The action to take when a duplicate file is found
+
+    Returns
+    -------
+    Dictionary with the following keys: values
+        'resources_ignored' : Array of string file paths of files that were ignored when
+        uploading the resource. Path should have the same base as resource_main_dir.
+                                Example:
+                                    ['path/to/ignored/file.pg', 'another/ignored/file.jpg']
+
+        'resources_updated' : Array of string file paths of files that were updated when
+         uploading the resource. Path should have the same base as resource_main_dir.
+                                 Example:
+                                    ['path/to/updated/file.jpg']
+        'action_metadata': Dictionary containing action metadata. Must be in the following format:
+                            {
+                                'destinationUsername': 'some_username'
+                            }
+        'file_metadata_list': List of dictionaries for each file that contains metadata
+                              and hash info. Must be in the following format:
+                                {
+                                    "actionRootPath": '/path/on/disk',
+                                    "destinationPath": '/path/on/target/destination',
+                                    "title": 'file_title',
+                                    "destinationHash": {'hash_algorithm': 'the_hash'}}
+                                }
+        'project_id': ID of the parent project for this upload. Needed for metadata upload.
+    """
+    try:
+        headers, username = validation_check(token)
+    except PresQTResponseException:
+        raise PresQTResponseException("Token is invalid. Response returned a 401 status code.",
+                                      status.HTTP_401_UNAUTHORIZED)
+
+    os_path = next(os.walk(resource_main_dir))
+
+    # Upload a new project
+    if not resource_id:
+        # Create a new project with the name being the top level directory's name.
+        project_title = os_path[1][0]
+        project_name, project_id = create_project(project_title, headers)
