f4d19ab666b099434e2d9da09b15674594377583
Justin Branco <jbranco@nd.edu>
2020-07-29 16:46:39 -0400

    PR changes

--- presqt/targets/curate_nd/utilities/utils/get_page_numbers.py
+++ presqt/targets/curate_nd/utilities/utils/get_page_numbers.py
@@ -23,9 +23,12 @@ def get_page_numbers(url, token):
     next_page = None
     previous_page = None
     total_pages = pagination_info['lastPage'].partition('=')[2]
+    
+    # If there is only 1 page of results, instead of a number Curate returns 'self'
     if total_pages == 'self':
         total_pages = '1'
 
+    # We are using partitions to pull the page numbers out of the links that Curate is returning
     if 'previousPage' in pagination_info.keys():
         previous_page = pagination_info['previousPage'].partition('=')[2]
     if 'nextPage' in pagination_info.keys():
