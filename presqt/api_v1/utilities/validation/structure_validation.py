78a0d93944c7bf34a810511f8c4a15c9f8d0a4b4
Justin Branco <jbranco@nd.edu>
2020-08-11 15:40:34 -0400

    Added clarifying message for transfer error

--- presqt/api_v1/utilities/validation/structure_validation.py
+++ presqt/api_v1/utilities/validation/structure_validation.py
@@ -22,7 +22,12 @@ def structure_validation(instance):
             "PresQT Error: Repository is not formatted correctly. Multiple directories exist at the top level.",
             status.HTTP_400_BAD_REQUEST)
 
-    if len(files) > 0 and instance.destination_resource_id is None:
+    if len(files) > 0 and instance.destination_resource_id is None and instance.action == 'resource_upload':
         raise PresQTResponseException(
-            "PresQT Error: Repository is not formatted correctly. Files exist at the top level.",
+            "PresQT Error: Repository is not formatted correctly.",
+            status.HTTP_400_BAD_REQUEST)
+
+    if len(files) > 0 and instance.destination_resource_id is None and instance.action == 'resource_transfer_in':
+        raise PresQTResponseException(
+            "PresQT Error: You need to select a resource to transfer into, as a single file can not be uploaded as a new project.",
             status.HTTP_400_BAD_REQUEST)
