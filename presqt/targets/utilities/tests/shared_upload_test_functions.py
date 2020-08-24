f5f5f7e1e67175cb242d25ec2c0af3385b9e4cf1
Justin Branco <jbranco@nd.edu>
2020-08-24 10:35:40 -0400

    Github tests are working

--- presqt/targets/utilities/tests/shared_upload_test_functions.py
+++ presqt/targets/utilities/tests/shared_upload_test_functions.py
@@ -5,7 +5,7 @@ from presqt.utilities import read_file
 
 def process_wait(process_info, ticket_path):
     # Wait until the spawned off process finishes in the background to do further validation
-    while process_info['status'] == 'in_progress':
+    while process_info['resource_upload']['status'] == 'in_progress':
         try:
             process_info = read_file('{}/process_info.json'.format(ticket_path), True)
         except json.decoder.JSONDecodeError:
@@ -52,8 +52,7 @@ def shared_upload_function_github(test_case_instance):
     response = test_case_instance.client.post(test_case_instance.url, {'presqt-file': open(
         test_case_instance.file, 'rb')}, **test_case_instance.headers)
 
-    ticket_number = response.data['ticket_number']
-    test_case_instance.ticket_path = 'mediafiles/uploads/{}'.format(ticket_number)
+    test_case_instance.ticket_path = 'mediafiles/jobs/{}'.format(test_case_instance.ticket_number)
 
     # Verify status code and message
     test_case_instance.assertEqual(response.status_code, 202)
@@ -62,7 +61,7 @@ def shared_upload_function_github(test_case_instance):
 
     # Verify process_info file status is 'in_progress' initially
     process_info = read_file('{}/process_info.json'.format(test_case_instance.ticket_path), True)
-    test_case_instance.assertEqual(process_info['status'], 'in_progress')
+    test_case_instance.assertEqual(process_info['resource_upload']['status'], 'in_progress')
 
     # Wait until the spawned off process finishes in the background to do further validation
     process_wait(process_info, test_case_instance.ticket_path)
@@ -70,16 +69,19 @@ def shared_upload_function_github(test_case_instance):
     # Verify process_info.json file data
     process_info = read_file('{}/process_info.json'.format(test_case_instance.ticket_path), True)
 
-    test_case_instance.assertEqual(process_info['status'], 'finished')
-    test_case_instance.assertEqual(process_info['message'], test_case_instance.process_message)
-    test_case_instance.assertEqual(process_info['status_code'], '200')
-    test_case_instance.assertEqual(process_info['failed_fixity'], test_case_instance.failed_fixity)
+    test_case_instance.assertEqual(process_info['resource_upload']['status'], 'finished')
     test_case_instance.assertEqual(
-        process_info['resources_ignored'], test_case_instance.resources_ignored)
+        process_info['resource_upload']['message'], test_case_instance.process_message)
+    test_case_instance.assertEqual(process_info['resource_upload']['status_code'], '200')
     test_case_instance.assertEqual(
-        process_info['resources_updated'], test_case_instance.resources_updated)
+        process_info['resource_upload']['failed_fixity'], test_case_instance.failed_fixity)
     test_case_instance.assertEqual(
-        process_info['hash_algorithm'], test_case_instance.hash_algorithm)
+        process_info['resource_upload']['resources_ignored'], test_case_instance.resources_ignored)
+    test_case_instance.assertEqual(
+        process_info['resource_upload']['resources_updated'], test_case_instance.resources_updated)
+    test_case_instance.assertEqual(
+        process_info['resource_upload']['hash_algorithm'], test_case_instance.hash_algorithm)
+
 
 def shared_upload_function_gitlab(test_case_instance):
     test_case_instance.headers['HTTP_PRESQT_FILE_DUPLICATE_ACTION'] = test_case_instance.duplicate_action
