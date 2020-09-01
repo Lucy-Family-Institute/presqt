bc62f587148ed893ef110043377e01ce902473c9
Justin Branco <jbranco@nd.edu>
2020-09-01 17:19:24 -0400

    Figshare refactored

--- presqt/targets/figshare/tests/views/resource/test_resource.py
+++ presqt/targets/figshare/tests/views/resource/test_resource.py
@@ -28,7 +28,7 @@ class TestResourceGETJSON(SimpleTestCase):
         self.client = APIClient()
         self.header = {'HTTP_PRESQT_SOURCE_TOKEN': FIGSHARE_TEST_USER_TOKEN}
         self.keys = ['kind', 'kind_name', 'id', 'title', 'date_created', 'date_modified', 'hashes',
-                     'extra', 'links', 'actions']
+                     'extra', 'children', 'links', 'actions']
 
     def test_success_project(self):
         """
