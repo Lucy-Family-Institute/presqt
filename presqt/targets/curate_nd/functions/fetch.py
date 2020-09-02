ca4c1512ebde97e56055c13cb3bae86630095224
Justin Branco <jbranco@nd.edu>
2020-09-02 10:18:45 -0400

    CurateND refactor

--- presqt/targets/curate_nd/functions/fetch.py
+++ presqt/targets/curate_nd/functions/fetch.py
@@ -139,6 +139,17 @@ def curate_nd_fetch_resource(token, resource_id):
         )
     # Get the resource
     resource = get_curate_nd_resource(resource_id, curate_instance)
+    children = []
+    if resource.kind == 'container':
+        # Get the children of this item
+        for child in resource.extra['containedFiles']:
+            children.append({
+                'kind': 'item',
+                'kind_name': 'file',
+                'id': child['id'],
+                'container': resource.id,
+                'title': child['label']})
+
     resource_dict = {
         "kind": resource.kind,
         "kind_name": resource.kind_name,
@@ -148,6 +159,6 @@ def curate_nd_fetch_resource(token, resource_id):
         "date_modified": resource.modified,
         "hashes": {"md5": resource.md5},
         "extra": resource.extra,
-        "children": []}
+        "children": children}
 
     return resource_dict
