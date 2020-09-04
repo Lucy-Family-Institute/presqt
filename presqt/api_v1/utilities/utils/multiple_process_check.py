4c98ba934f47d961606a2d36fa69e38d5e0ecd21
Justin Branco <jbranco@nd.edu>
2020-09-04 11:47:29 -0400

    Added docstring

--- presqt/api_v1/utilities/utils/multiple_process_check.py
+++ presqt/api_v1/utilities/utils/multiple_process_check.py
@@ -5,6 +5,16 @@ from presqt.utilities import read_file
 
 def multiple_process_check(process_info_path):
     """
+    Check in on if a user currrently has an action in progress.
+
+    Parameters
+    ----------
+    process_info_path: str
+        The path to the user's action contents
+
+    Returns
+    -------
+        Whether the user has an action in progress or not (bool)
     """
     # Check if the file exists
     if os.path.exists(process_info_path):
