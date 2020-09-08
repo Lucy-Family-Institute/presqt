303b5f8866e708272777964a06ab3df2b5de36ad
Justin Branco <jbranco@nd.edu>
2020-09-08 11:12:41 -0400

    Catch decode errors

--- presqt/api_v1/utilities/utils/multiple_process_check.py
+++ presqt/api_v1/utilities/utils/multiple_process_check.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from presqt.utilities import read_file
 
@@ -19,8 +20,13 @@ def multiple_process_check(process_info_path):
     process_info_file = "{}/process_info.json".format(process_info_path)
     # Check if the file exists
     if os.path.exists(process_info_file):
-        # Check if this user currently has any other process in progress
-        process_info_data = read_file(process_info_file, True)
+        process_info_data = None
+        while not process_info_data:
+            try:
+                # Check if this user currently has any other process in progress
+                process_info_data = read_file(process_info_file, True)
+            except json.decoder.JSONDecodeError:
+                pass
         # Loop through the dictionaries and check the status's
         for key, value in process_info_data.items():
             if value['status'] == 'in_progress':
