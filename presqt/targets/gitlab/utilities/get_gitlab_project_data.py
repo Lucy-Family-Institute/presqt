df4b3459723fbce89016a5089f9fbeb862d7a18d
Justin Branco <jbranco@nd.edu>
2020-09-10 11:20:24 -0400

    Remove collection Progress - GitLab

--- presqt/targets/gitlab/utilities/get_gitlab_project_data.py
+++ presqt/targets/gitlab/utilities/get_gitlab_project_data.py
@@ -1,9 +1,6 @@
-import urllib.parse
-
-from presqt.utilities import update_process_info, increment_process_info
 
 
-def get_gitlab_project_data(initial_data, headers, resources, process_info_path):
+def get_gitlab_project_data(initial_data, headers, resources):
     """
     Get all directories and files of given projects.
 
@@ -23,12 +20,6 @@ def get_gitlab_project_data(initial_data, headers, resources, process_info_path)
     A list of resources.
 
     """
-    from presqt.targets.gitlab.utilities.gitlab_paginated_data import gitlab_paginated_data
-
-    # Add the total number of projects to the process info file.
-    # This is necessary to keep track of the progress of the request.
-    update_process_info(process_info_path, len(initial_data), 'resource_collection', 'fetch')
-
     for project in initial_data:
         if ('marked_for_deletion_at' in project.keys() and not project['marked_for_deletion_at']) or (
                 'marked_for_deletion_at' not in project.keys()):
@@ -38,8 +29,5 @@ def get_gitlab_project_data(initial_data, headers, resources, process_info_path)
                 "container": None,
                 "id": project['id'],
                 "title": project['name']})
-
-            # Increment the number of files done in the process info file.
-            increment_process_info(process_info_path, 'resource_collection', 'fetch')
 
     return resources
