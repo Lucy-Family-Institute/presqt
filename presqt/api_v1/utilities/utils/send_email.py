25af46aefc96f92026ce279c139ae77e44146be2
Justin Branco <jbranco@nd.edu>
2020-09-14 16:41:59 -0400

    Neew email address

--- presqt/api_v1/utilities/utils/send_email.py
+++ presqt/api_v1/utilities/utils/send_email.py
@@ -21,7 +21,7 @@ def transfer_upload_email_blaster(email_address, action):
     send_mail(
         'PresQT Action Complete',
         'The {} you started on PresQT has finished.'.format(real_action),
-        'presqt@nd.edu',
+        'noreply@presqt.crc.nd.edu',
         [email_address],
         fail_silently=True)
 
@@ -41,7 +41,7 @@ def download_email_blaster(email_address, file_path):
     message = EmailMessage(
         'PresQT Action Complete',
         'The download you started on PresQT has finished. It has been attached to this email.',
-        'presqt@nd.edu',
+        'noreply@presqt.crc.nd.edu',
         [email_address]
     )
     message.attach_file(file_path)
