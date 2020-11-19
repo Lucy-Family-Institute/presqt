5be39906ce80171adc3f3e22a520bfa8900dd7c9
Justin Branco <jbranco@nd.edu>
2020-11-19 15:16:01 -0500

    Added correapi to reqs, updated management command. Preliminary work

--- presqt/management/commands/fairshake.py
+++ presqt/management/commands/fairshake.py
@@ -1,22 +1,49 @@
 import json
+import coreapi
 
 import requests
 from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
-  def handle(self, *args, **kwargs):
-    fairshake_url = 'https://fairshake.cloud/digital_object/?format=api'
-    headers = {'Authorization': 'token d9fb202a16298dfc4fa098be452c49e23861d160',
-               'Content-Type': 'application/json',
-               'accept': 'application/json'}
-    data = {
-      "projects": [
-        116
-      ],
-      "title": "presqt-ui",
-      "url": "https://github.com/ndlib/presqt-ui"
-    }
 
-    response = requests.post(fairshake_url, headers=headers, data=data)
-    print(response.status_code)
+    def handle(self, *args, **kwargs):
+        api_key = 'd9fb202a16298dfc4fa098be452c49e23861d160'
+        project_id = 116
+        client = coreapi.Client(auth=coreapi.auth.TokenAuthentication(
+            token=api_key, scheme='token'))
+        schema = client.get('https://fairshake.cloud/coreapi/')
+
+        ### THIS CREATES THE DIGITAL_OBJECT ###
+        obj = client.action(schema, ['digital_object', 'create'], params=dict(
+            url='https://github.com/ndlib/presqt-ui',
+            title='presqt-ui',
+            projects=[project_id],
+            type='tool'
+        ))
+        ### DIGITAL_OBJECT ID ###
+        obj_id = obj['id']
+
+        # obj at this point =
+        # {
+        #   "id": <obj_id>,
+        #   "projects": [116],
+        #   "description": "",
+        #   "image": "",
+        #   "tags": "",
+        #   "type": "tool",
+        #   "title": "presqt-ui",
+        #   "url": "https://github.com/ndlib/presqt-ui",
+        #   "authors": [224],
+        #   "rubrics": []
+        # }
+        # print(obj_id)
+
+        # TODO: ADD CODE TO RUN ASSESSMENTS HERE
+
+        # This should be the call to get scores
+        # Replace target with obj_id
+        score = client.action(schema, ['score', 'list'], params=dict(
+            target=obj_id
+        ))
+        print(score)
